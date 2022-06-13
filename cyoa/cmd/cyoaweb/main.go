2a8672955ff8bfa8778cc18eb89ca5f3400eddac
Adwithya Magow <amagow@finverse.com>
2022-06-14 02:43:03 +0800

    Add option to give template

--- cyoa/cmd/cyoaweb/main.go
+++ cyoa/cmd/cyoaweb/main.go
@@ -25,7 +25,7 @@ func main() {
 		panic(err)
 	}
 
-	h := core.NewHandler(story)
+	h := core.NewHandler(story, nil)
 	fmt.Printf("Starting the server on the port: %d\n", *port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 
