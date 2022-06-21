213275b6dc475d2885734f04da561fde8ae0d9f5
Adwithya Magow <amagow@finverse.com>
2022-06-22 02:16:45 +0800

    Write dfs link parser

--- link/internal/link-core/link.go
+++ link/internal/link-core/link.go
@@ -1,6 +1,11 @@
 package link
 
-import "io"
+import (
+	"fmt"
+	"io"
+
+	"golang.org/x/net/html"
+)
 
 // Link represents a link (<a href=".." />) in an HTML
 // document
@@ -12,6 +17,24 @@ type Link struct {
 // Parse will take in an HTML document and return a slice
 // of parsed links
 func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		panic(err)
+	}
+
+	var dfs func(*html.Node, string)
+	dfs = func(n *html.Node, padding string) {
+		fmt.Println(padding, n.Data)
+		if n.FirstChild != nil {
+			dfs(n.FirstChild, padding+"  ")
+		}
+
+		if n.NextSibling != nil {
+			dfs(n.NextSibling, padding)
+		}
+
+	}
+	dfs(doc, "")
 
 	return []Link{}, nil
 }
