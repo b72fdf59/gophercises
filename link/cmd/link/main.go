9b88c51a88912a101aac6cc471ec68ad2ebb0a04
Adwithya Magow <amagow@finverse.com>
2022-06-22 01:51:42 +0800

    Update main to use link package

--- link/cmd/link/main.go
+++ link/cmd/link/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"link/internal/link-core"
+	"os"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	filename := flag.String("file", "examples/ex1.html", "The HTML file to parse")
+	flag.Parse()
+	fmt.Printf("Using the file %s\n", *filename)
+
+	f, err := os.Open(*filename)
+	if err != nil {
+		panic(err)
+	}
+
+	links, err := link.Parse(f)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%v\n", links)
 }
